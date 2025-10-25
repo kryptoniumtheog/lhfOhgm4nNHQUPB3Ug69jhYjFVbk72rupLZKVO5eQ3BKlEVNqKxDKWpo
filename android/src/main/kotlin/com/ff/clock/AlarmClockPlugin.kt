61e27660e007aae146528c8dbd6b1e34f6dd74cb
kryptoniumtheog <149662362+kryptoniumtheog@users.noreply.github.com>
2025-10-25 15:25:12 +0300

    fixed other issues

--- android/src/main/kotlin/com/ff/clock/AlarmClockPlugin.kt
+++ android/src/main/kotlin/com/ff/clock/AlarmClockPlugin.kt
@@ -2,6 +2,7 @@ package com.ff.clock
 
 import android.app.AlarmManager
 import android.app.PendingIntent
+import android.app.NotificationManager   // <-- add this
 import android.content.Context
 import android.content.Intent
 import android.os.Build
